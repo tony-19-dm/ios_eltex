1591981569a9dee5dbb0029f2489285ac74264d1
tony_19 <admitriev377@gmail.com>
2026-04-26 10:44:44 +0700

    Homework 13 v1.0002

--- Market/Market/Screens/HistoryViewController.swift
+++ Market/Market/Screens/HistoryViewController.swift
@@ -154,10 +154,8 @@ extension HistoryViewController: UITableViewDataSource {
         
         let item = botResults[indexPath.row]
         
-        let sign = item.income > 0 ? "+" : ""
-        
         cell.textLabel?.numberOfLines = 0
-        cell.textLabel?.text = "\(item.botName) (\(item.pair)), day = \(item.day), income = \(sign)\(Int(item.income))$"
+        cell.textLabel?.text = "\(item.botName) (\(item.pair)), day = \(item.day), income = +\(Int(item.income))$"
         
         return cell
     }
