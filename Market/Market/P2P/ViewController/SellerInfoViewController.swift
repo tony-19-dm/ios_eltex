72a34a9ba6b862b340f59d9b6fd7b8162ac72bd1
tony_19 <admitriev377@gmail.com>
2026-05-09 11:46:38 +0700

    Homework 17 v1.00002

--- Market/Market/P2P/ViewController/SellerInfoViewController.swift
+++ Market/Market/P2P/ViewController/SellerInfoViewController.swift
@@ -33,7 +33,8 @@ final class SellerInfoViewController: UIViewController {
         title = "Продавец"
         view.backgroundColor = .systemBackground
         setupUI()
-        populate()
+        makeConstraints()
+        addData()
     }
 
     private func setupUI() {
@@ -41,39 +42,41 @@ final class SellerInfoViewController: UIViewController {
         avatarImageView.tintColor = .systemMint
         avatarImageView.contentMode = .scaleAspectFit
         avatarImageView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         stackView.axis = .vertical
         stackView.spacing = 12
         stackView.alignment = .center
         stackView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         [nameLabel, idLabel, rateLabel, reserveLabel,
          ratingLabel, tradesLabel, memberLabel].forEach {
             $0.textAlignment = .center
             $0.numberOfLines = 0
             stackView.addArrangedSubview($0)
         }
-
+        
         nameLabel.font = .boldSystemFont(ofSize: 22)
         idLabel.font = .systemFont(ofSize: 13)
         idLabel.textColor = .secondaryLabel
-
+        
         view.addSubview(avatarImageView)
         view.addSubview(stackView)
-
+    }
+    
+    private func makeConstraints() {
         NSLayoutConstraint.activate([
             avatarImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
             avatarImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             avatarImageView.widthAnchor.constraint(equalToConstant: 80),
             avatarImageView.heightAnchor.constraint(equalToConstant: 80),
-
+            
             stackView.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 16),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
         ])
     }
 
-    private func populate() {
+    private func addData() {
         nameLabel.text = viewModel.sellerName
         idLabel.text = "ID: \(viewModel.sellerId)"
         rateLabel.text = "Курс: \(viewModel.rate)"
